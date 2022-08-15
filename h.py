fdb44fa549cec9100900e3a233ea2b33502c0d5f
Victor San Kho Lin <victor@sankholin.com>
2022-08-15 12:57:18 +1000

    Fixed CodeCommit exclusion

--- h.py
+++ h.py
@@ -2,18 +2,23 @@
 import ast
 from glob import glob
 
+
+def run(file_obj):
+    code = ast.parse(file_obj.read())
+    for node in ast.walk(code):
+        if isinstance(node, (ast.FunctionDef, ast.ClassDef, ast.Module)):
+            docstring = ast.get_docstring(node)
+            if docstring:
+                print(docstring)
+                # print(repr(docstring))
+
+
 if __name__ == '__main__':
     # print(glob('tests/test_*'))
 
-    for file in glob('tests/test_*'):
-        with open(file, encoding='utf-8') as f:
-            code = ast.parse(f.read())
-            for node in ast.walk(code):
-                if isinstance(node, (ast.FunctionDef, ast.ClassDef, ast.Module)):
-                    docstring = ast.get_docstring(node)
-                    if docstring:
-                        print(docstring)
-                        # print(repr(docstring))
+    for filename in glob('tests/test_*'):
+        with open(filename, encoding='utf-8') as f:
+            run(f)
 
 # Usage:
 #   python h.py > h.bat
